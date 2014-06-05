675034c7ba4a27396a19e02d285b0fbb574f1c9c
Raya Desawade <rayalynn@gmail.com>
2014-06-05 09:20:41 -0700

    Fixes for handling festivals/music concerts
    * Fix bug where incorrect artist name is used when event type is
    festival.
    * Format concert data sent back so unnecessary info (ex. multiple
    performances for festivals for each artist match) is not sent.

--- app/utils/integration.js
+++ app/utils/integration.js
@@ -1,21 +1,36 @@
 /*
  integration.js
  */
+var _ = require('lodash');
 
 // Return an array of concerts where the artist id in concerts matches
 // an artist id in artist_ids.
-// users_artist key is set to the artist the user selected
 exports.filterConcertMatches = function( artist_ids, concerts ) {
-  var matches = [];
+  var artist_id,
+      artist_name,
+      matches = [];
+
   for ( var i = 0; i < concerts.length; i++ ) {
     for ( var j = 0; j < concerts[i].performance.length; j++ ) {
-      var artist_id = String(concerts[i].performance[j].artist.id);
+      artist_id = String(concerts[i].performance[j].artist.id);
+      artist_name = concerts[i].performance[j].artist.displayName;
       if ( artist_ids[artist_id] ) {
-        concerts[i].users_artist = concerts[i].performance[j].artist.displayName;
-        matches.push(concerts[i]);
+        matches.push(formatConcertData(artist_ids[artist_id], concerts[i]));
       }
     }
   }
   return matches;
 };
 
+// Saves necessary concert data to object
+function formatConcertData( users_artist, concert ) {
+  var entry = {};
+
+  entry.location = concert.location;
+  entry.start = concert.start;
+  entry.type = concert.type;
+  entry.uri = concert.uri;
+  entry.users_artist = users_artist;
+  entry.venue = concert.venue;
+  return entry;
+}
--- spec/integration_spec.js
+++ spec/integration_spec.js
@@ -3,27 +3,27 @@ var integration = require('../app/utils/integration');
 describe('integration', function() {
   describe('matching concerts with artist ids', function() {
     var artist_ids, concerts;
-    before(function() {
+    beforeEach(function() {
       artist_ids = {
         '100' : 'Artist 1',
         '101' : 'Artist 2',
-        '102' : 'Artist 3'
+        '102' : 'Artist 3',
+        '103' : 'Modest Mouse'
       };
       concerts = [
         { type : 'Concert',
           displayName : 'Artist 1 + Artist 2 Concert',
           id : 8888,
           performance : [
-            { billing : 'headline',
+            {
               artist : { identifier : [],
                 id : 100,
                 displayName : 'Artist 1' }},
-            { billing : 'support',
-              billingIndex : 2,
+            {
               id : 8889,
               artist : { identifier : [],
                 id : 200,
-                displayName : 'Artist 200' }}
+                displayName : 'Non Followed Artist' }}
           ]},
         { type : 'Concert',
           displayName : 'Artist 3 Concert',
@@ -37,11 +37,9 @@ describe('integration', function() {
           ]}
       ];
     });
-    it('should return matchees', function() {
+    it('should return matches', function() {
       var matches = integration.filterConcertMatches(artist_ids, concerts);
       expect(matches).to.have.length(2);
-      expect(matches).to.include(concerts[0]);
-      expect(matches).to.include(concerts[1]);
     });
     it('should return an empty array if there are no matches', function() {
       var no_match_artist_ids = {
@@ -55,6 +53,21 @@ describe('integration', function() {
       var matches = integration.filterConcertMatches(artist_ids, concerts);
       expect(matches[0].users_artist).to.equal('Artist 1');
       expect(matches[1].users_artist).to.equal('Artist 3');
+    });
+    it('should save the correct users_artist when there are multiple performance objects', function() {
+      var extra_performance = {
+        billing : 'headline',
+        artist : {
+          identifier : [],
+          id : 103,
+          displayName : 'Modest Mouse'
+        }
+      };
+      concerts[0].performance.push( extra_performance );
+      var matches = integration.filterConcertMatches( artist_ids, concerts);
+      expect(matches[0].users_artist).to.equal('Artist 1');
+      expect(matches[1].users_artist).to.equal('Modest Mouse');
+      expect(matches[2].users_artist).to.equal('Artist 3');
     });
   });
 
