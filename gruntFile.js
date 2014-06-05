7ffa1b1ec769f5993297d12820242db616fea0af
Raya Desawade <rayalynn@gmail.com>
2014-06-05 15:00:42 -0700

    Switch to using SASS
    * Add grunt job to watch scss files
    * Add grunt job to combine css files
    * Use bower to dl Pure CSS framework.

--- gruntFile.js
+++ gruntFile.js
@@ -1,7 +1,20 @@
 module.exports = function( grunt ) {
+
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-sass');
 
   grunt.initConfig({
+    cssmin: {
+      combine: {
+        files: {
+          'app/public/stylesheets/app.min.css': ['bower_components/pure/pure-min.css',
+            'bower_components/pure/grids-responsive-min.css',
+            'build/app.css' ]
+        }
+      }
+    },
     mochaTest : {
       test : {
         options : {
@@ -18,9 +31,29 @@ module.exports = function( grunt ) {
         },
         src : ['spec/integration/*.js']
       }
+    },
+    sass: {
+      dist: {
+        options: {
+          outputStyle: 'nested'
+        },
+        files: {
+          'build/app.css' : 'app/views/styles/app.scss'
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['app/views/styles/*.scss'],
+        tasks: ['sass:dist','cssmin:combine'],
+        options: {
+          spawn: false
+        }
+      }
     }
   });
 
+  grunt.registerTask('build:dev', 'sass:dist', 'cssmin:combine');
   grunt.registerTask('test', 'mochaTest:test');
   grunt.registerTask('test:client', 'mochaTest:integration');
 
