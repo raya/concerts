a2bd1d8ee265f41a8282efe691fba57098800a89
Raya Desawade <rayalynn@gmail.com>
2014-06-10 08:54:02 -0700

    Move rdio callback url to environment variable for easier configuration on servers

--- app/config/config.js
+++ app/config/config.js
@@ -16,7 +16,7 @@ config.ECHONEST_API_URL   = 'http://developer.echonest.com/api/v4/';
 
 config.RDIO_AUTHORIZATION_URL = 'https://www.rdio.com/oauth2/authorize';
 config.RDIO_API_URL = 'https://www.rdio.com/api/1/';
-config.RDIO_CALLBACK_URL = 'http://127.0.0.1:5000/users/authorize/rdio/callback';
+config.RDIO_CALLBACK_URL = process.env.RDIO_CALLBACK_URL;
 config.RDIO_CLIENT_ID = process.env.RDIO_CLIENT_ID;
 config.RDIO_CLIENT_SECRET = process.env.RDIO_CLIENT_SECRET;
 config.RDIO_TOKEN_URL = 'https://www.rdio.com/oauth2/token';
